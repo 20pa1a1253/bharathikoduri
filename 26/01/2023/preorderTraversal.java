ca7fd56fbcd20b67913e202a43bb25123d3cae91
20pa1a1253 <85443978+20pa1a1253@users.noreply.github.com>
2023-01-27 00:13:40 +0530

    Create preorderTraversal.java

---
+++ 26/01/2023/preorderTraversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        preorder(root,list);
+        return list;
+    }
+    public void preorder(TreeNode root,ArrayList<Integer>list){
+        if(root==null){
+            return ;
+        }
+        list.add(root.val);
+        preorder(root.left,list);
+        preorder(root.right,list);        
+        
+        
+    }
+}
